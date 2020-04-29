585bea6efebaa7f30fe91de8028eb6de36be13f2
Denis Molchanov <62193982+saggarth@users.noreply.github.com>
2020-04-30 07:06:30 +0800

    uploaded folder lesson_8

---
+++ lesson_8/client/Callback.java
@@ -0,0 +1,5 @@
+package lesson_8.client;
+
+public interface Callback {
+    void callback(Object... args);
+}
---
+++ lesson_8/server/MainServer.java
@@ -0,0 +1,7 @@
+package lesson_8.server;
+
+public class MainServer {
+    public static void main(String[] args) {
+        new Server();
+    }
+}
